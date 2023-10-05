5dfaa31f9427cf5f9647176a7c7b3cdb0eabe4f0
Priyanka Ojha <priyankaojha.cse@gmail.com>
2023-10-04 20:50:05 -0700

    Practice

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,5 @@
 package LinkedList;
-
+import java.util.*;
 public class LinkedList {
     Node head;
 
@@ -44,6 +44,39 @@ public class LinkedList {
 
     }
 
+    public static void removeDups(LinkedList list){
+        int[] arr = new int[100];
+        Arrays.fill(arr,0);
+        int i=0;
+        Node curr  =list.head;
+        Node prev = null;
+        while(curr!=null){
+            int val = curr.data;
+            arr[val]+=1;
+            if(arr[val]>1){
+                prev.next = curr.next;
+                curr.next = null;
+            }
+            prev=curr;
+            curr=curr.next;
+        }}
+        public static void approachtwoRemoveDups(LinkedList list){
+            Node current = list.head;
+            while (current != null) {
+                Node runner = current;
+                while (runner.next != null) {
+                    if (runner.next.data == current.data) {
+                        runner.next = runner.next.next;
+                    } else {
+                        runner = runner.next;
+                    }
+                }
+                current = current.next;
+            }
+        }
+
+
+
     public static void main(String[] args){
         LinkedList list = new LinkedList();
         list.insert(list,1);
@@ -51,6 +84,9 @@ public class LinkedList {
         list.insert(list,3);
         list.insert(list,4);
         list.insert(list,5);
+        list.insert(list,5);
+       // removeDups(list);
+        approachtwoRemoveDups(list);
         list.printList(list);
     }
 
