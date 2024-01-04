982e63e8de5a3463e76ffa20f6d211c96f8e3da6
Priyanka Ojha <priyankaojha.cse@gmail.com>
2024-01-03 20:19:10 -0700

    Practice

---
+++ StacksAndQueues/Circularqueue.java
@@ -0,0 +1,81 @@
+package StacksAndQueues;
+
+public class Circularqueue {
+
+    private int[] arr;
+    private int head;
+    private int tail;
+    private int length;
+
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        head=-1;
+        tail=-1;
+        length=k;
+
+    }
+
+    public boolean enQueue(int value) {
+
+        if(isFull()){
+            return false;
+        }
+        if(isEmpty()){
+            head=0;
+
+        }
+        tail=(tail+1)%length;
+        arr[tail]=value;
+        return true;
+
+    }
+
+    public boolean deQueue() {
+        if(isEmpty()){
+            return false;
+        }
+        if(head==tail){
+            head=-1;
+            tail=-1;
+            return true;
+
+        }
+        head = (head+1)%length;
+        return true;
+
+    }
+
+    public int Front() {
+
+        if(isEmpty()){
+            return -1;
+        }
+
+        return arr[head];
+
+
+
+    }
+
+    public int Rear() {
+
+        if(isEmpty()){
+            return -1;
+        }
+
+        return arr[tail];
+
+    }
+
+    public boolean isEmpty() {
+
+        return head==-1;
+
+    }
+
+    public boolean isFull() {
+
+        return (tail+1)%length == head;
+
+    }
+}
